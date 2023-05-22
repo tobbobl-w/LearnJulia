a8452a6f94feb03a8962275ab70c1f3bafdbc858
Tobias Leigh-Wood <tjleighwood@gmail.com>
2023-05-22 18:35:40 +0100

    Add first test

--- src/LearnJulia.jl
+++ src/LearnJulia.jl
@@ -1,5 +1,15 @@
 module LearnJulia
 
 # Write your package code here.
+using Expectations, Distributions
+
+function foo(μ=1.0, σ=2.0)
+    println("modified for fun")
+    d = Normal(μ, σ)
+    E = expectation(d)
+    return E(x -> sin(x))
+end
+
+export foo
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,5 @@ using Test
 
 @testset "LearnJulia.jl" begin
     # Write your tests here.
+    @test foo(0) < 1E-16
 end
